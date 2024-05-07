ba19a4072387f95162d9de6445c65696def0f7dd
Pablo Yohan <pabloyohandias@gmail.com>
2024-05-07 08:56:49 -0300

    Criação do projeto

---
+++ src/test/java/br/com/pabloyohan/primeiroprojetospring/PrimeiroProjetoSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pabloyohan.primeiroprojetospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroProjetoSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
