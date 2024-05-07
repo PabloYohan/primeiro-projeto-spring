c4916e6d2b6a0e058ab9e354dd6687101427a32f
Pablo Yohan <pabloyohandias@gmail.com>
2024-05-07 09:38:18 -0300

    Entendendo sobre o component scan

--- src/main/java/br/com/pabloyohan/controllers/PrimeiroController.java
+++ src/main/java/br/com/pabloyohan/controllers/PrimeiroController.java
@@ -1,4 +1,4 @@
-package br.com.pabloyohan.primeiroprojetospring;
+package br.com.pabloyohan.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
