89ee09c5841d930491b7beba1ce9d2d49ffb6be6
Pablo Yohan <pabloyohandias@gmail.com>
2024-05-21 11:16:15 -0300

    Entendendo sobre pathParams

--- src/main/java/br/com/pabloyohan/controllers/PrimeiroController.java
+++ src/main/java/br/com/pabloyohan/controllers/PrimeiroController.java
@@ -2,7 +2,14 @@ package br.com.pabloyohan.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
 
 
 @RestController
@@ -13,6 +20,23 @@ public class PrimeiroController {
   public String primeiroMetdodo() {
       return "Meu Primeiro Método ApiREST";
   }
+
+  @GetMapping("/primeiroMetodo/{id}")
+  public String segundoMetodo(@PathVariable String id) {
+      return "O parametro é " + id;
+  }
+
+  @GetMapping("/metodoQueryParams")
+  public String metodoComQueryParam(@RequestParam String id) {
+      return "O id do metodoComQueryParam é " + id;
+  }
+
+  @GetMapping("/metodoQueryParams2")
+  public String metodoComQueryParam2(@RequestParam Map<String, String> allParams) {
+      return "O id do metodoComQueryParam é " + allParams.entrySet();
+  }
+  
+  
 
 
 }
